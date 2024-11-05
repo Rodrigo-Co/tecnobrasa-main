77e6a4bb82b866fcfacdd35c522a86d4c7b726f3
Rodrigo-Co <141284747+Rodrigo-Co@users.noreply.github.com>
2024-11-05 13:31:45 -0300

    linhainicio

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root', // Substitua pelo seu usuário do MySQL
-    password: 'rodrigo', // Substitua pela sua senha do MySQL
+    password: 'cimatec', // Substitua pela sua senha do MySQL
     database: 'bancotb', // Nome do seu banco de dados
 });
 
